e6e4bea394b5985927acc7f49c4e84fa1fee89d9
ajurasz <arkadiusz.jurasz@gmail.com>
2020-12-08 22:08:22 +0100

    day 8

---
+++ src/main/kotlin/day08/solution.kt
@@ -0,0 +1,80 @@
+package day08
+
+val INSTRUCTION_PATTERN = """([\w+]{3}) ([+-]\d+)""".toRegex()
+
+sealed class Instruction
+data class Acc(val value: Int) : Instruction()
+data class Jmp(val value: Int) : Instruction()
+data class Nop(val value: Int) : Instruction()
+
+sealed class Result(val value: Int)
+data class Success(private val _value: Int) : Result(_value)
+data class Failure(private val _value: Int) : Result(_value)
+
+typealias Code = ArrayList<Instruction>
+
+class Bootloader {
+    private var pointer = 0
+    private var accumulator = 0
+    private val registry = mutableSetOf<Int>()
+
+    fun run(code: Code, force: Boolean = true): Result {
+        while (registry.add(pointer)) {
+            if (force && code.size == pointer) {
+                return Success(accumulator)
+            } else {
+                runInstruction(code, pointer)
+            }
+        }
+        return Failure(accumulator)
+    }
+
+    private fun runInstruction(code: Code, index: Int) {
+        when (val instruction = code[index]) {
+            is Acc -> {
+                accumulator += instruction.value
+                pointer += 1
+            }
+            is Jmp -> pointer += instruction.value
+            is Nop -> pointer += 1
+        }
+    }
+}
+
+fun partA(lines: Collection<String>): Int {
+    val code = Code(lines.map(::toInstruction))
+    return Bootloader().run(code).value
+}
+
+fun partB(lines: Collection<String>): Int {
+    val code = Code(lines.map(::toInstruction))
+    (0..code.size).forEach { index ->
+        when (val result = Bootloader().run(newCode(index, code))) {
+            is Success -> return result.value
+        }
+    }
+    throw RuntimeException("Failed to find solution")
+}
+
+private fun toInstruction(line: String): Instruction {
+    val (symbol, value) = INSTRUCTION_PATTERN.find(line)!!.destructured
+    return when (symbol) {
+        "acc" -> Acc(value.toInt())
+        "jmp" -> Jmp(value.toInt())
+        else -> Nop(value.toInt())
+    }
+}
+
+private fun newCode(index: Int, code: Code) = code.mapIndexed { idx, instruction ->
+    if (index == idx) {
+        replaceInstruction(instruction)
+    } else {
+        instruction
+    }
+} as Code
+
+private fun replaceInstruction(instruction: Instruction) = when (instruction) {
+    is Jmp -> Nop(instruction.value)
+    is Nop -> Jmp(instruction.value)
+    else -> instruction
+}
--- src/test/kotlin/day08/SolutionSpec.kt
+++ src/test/kotlin/day08/SolutionSpec.kt
@@ -1,4 +1,4 @@
-package day06
+package day08
 
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
@@ -8,21 +8,21 @@ import load
 class SolutionSpec : Spek({
     describe("Part A") {
         it("should resolve test scenario") {
-            assertThat(partA(load("day06_test.txt"))).isEqualTo(11)
+            assertThat(partA(load("day08_test.txt"))).isEqualTo(5)
         }
 
         it("should resolve final scenario") {
-            assertThat(partA(load("day06.txt"))).isEqualTo(6625)
+            assertThat(partA(load("day08.txt"))).isEqualTo(2003)
         }
     }
 
     describe("Part B") {
         it("should resolve test scenario") {
-            assertThat(partB(load("day06_test.txt"))).isEqualTo(6)
+            assertThat(partB(load("day08_test.txt"))).isEqualTo(8)
         }
 
         it("should resolve final scenario") {
-            assertThat(partB(load("day06.txt"))).isEqualTo(3360)
+            assertThat(partB(load("day08.txt"))).isEqualTo(1984)
         }
     }
 })
