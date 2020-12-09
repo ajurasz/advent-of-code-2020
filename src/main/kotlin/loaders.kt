0675d07ad5bad43f6044e24890b730bdb1423d57
ajurasz <arkadiusz.jurasz@gmail.com>
2020-12-09 21:00:41 +0100

    fix data conversion in file loader

--- src/main/kotlin/loaders.kt
+++ src/main/kotlin/loaders.kt
@@ -1,3 +1,10 @@
 import java.io.File
 
-inline fun <reified T> load(file: String): List<T> = File({}::class.java.classLoader.getResource(file).toURI()).readLines().map { it as T }
+inline fun <reified T> load(file: String): List<T> =
+    File(Unit::class.java.classLoader.getResource(file).toURI()).readLines()
+        .map {
+            when (T::class) {
+                Long::class -> it.toLong()
+                else -> it
+            } as T
+        }
